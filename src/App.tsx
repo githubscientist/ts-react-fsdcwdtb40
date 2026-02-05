b776aac473a35427dd9b7b1d0bd6d9f244862a16
githubscientist <sathish.datascientist@gmail.com>
2026-02-05 20:07:58 +0530

    initial react scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// component: React components are reusable pieces of UI(either one or more than one html elements)
+// components can be created in two ways:
+// 1. functional component
+// 2. class component
+
+// functional component: App
+function App() {
+  return <h1>Hello World!</h1>
+}
+
+export default App;
