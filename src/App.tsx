801452f8f986c21004d345a2488489958ce895f5
githubscientist <sathish.datascientist@gmail.com>
2026-02-06 20:48:46 +0530

    functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-// component: React components are reusable pieces of UI(either one or more than one html elements)
+// component: React components are reusable pieces of UI (either one or more than one html elements)
 // components can be created in two ways:
 // 1. functional component
 // 2. class component
 
 // functional component: App
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <p>Welcome to my Website!</p>
+    </>
+  )
 }
 
 export default App;
