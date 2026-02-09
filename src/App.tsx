c335d193b434cb8afc31a227b541cdba3e127dd2
githubscientist <sathish.datascientist@gmail.com>
2026-02-09 18:20:06 +0530

    functional component --named function syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-// component: React components are reusable pieces of UI (either one or more than one html elements)
-// components can be created in two ways:
-// 1. functional component
-// 2. class component
-
-// functional component: App
-import { Component } from "react";
-class App extends Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
+function App() {
+  return <h1>Hello World</h1>
 }
 
 export default App;
