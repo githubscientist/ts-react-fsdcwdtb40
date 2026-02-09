ddf348d8cda416f3b3657d35b6fce95c2cc835b8
githubscientist <sathish.datascientist@gmail.com>
2026-02-09 18:54:47 +0530

    creating multiple components + passing data as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-const App = () => {
-  // data & js functions
-  let name: string = 'krish';
+type Props = {
+  name: string;
+}
+
+const Header = ({ name }: Props) => {
 
   return (
     <>
       <h1>Hello { name.slice(0, 1).toUpperCase() + name.slice(1, ) }!</h1>
+    </>
+  )
+}
+
+
+const App = () => {
+  let name: string = 'sathish';
+
+  return (
+    <>
+      <Header name={name} />
     </>
   )
 }
