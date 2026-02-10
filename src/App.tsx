ad4366761f6e70d48c5979201eecc9f6c673129e
githubscientist <sathish.datascientist@gmail.com>
2026-02-10 20:37:29 +0530

    working with arrays in state

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,24 @@ const App = () => {
   const [likes, setLikes] = useState<number>(0);
   const [dislikes, setDislikes] = useState<number>(0);
 
+  const [reactions, setReactions] = useState<string []>([]);
+
+  const handleLike = () => {
+    setLikes(likes + 1);
+    setReactions([...reactions, 'L']);
+  };
+
+  const handleDislike = () => {
+    setDislikes(dislikes + 1);
+    setReactions([...reactions, 'D']);
+  };
+
+  console.log(reactions);
+
   return (
     <>
-      <button onClick={() => setLikes(likes + 1)}>Like</button> &nbsp; { likes } &nbsp; likes &nbsp;
-      <button onClick={() => setDislikes(dislikes + 1)}>Dislike</button> &nbsp; { dislikes } &nbsp; dislikes &nbsp;
+      <button onClick={handleLike}>Like</button> &nbsp; { likes } &nbsp; likes &nbsp;
+      <button onClick={handleDislike}>Dislike</button> &nbsp; { dislikes } &nbsp; dislikes &nbsp;
     </>
   )
 }
