7559d7ab2af63fd186cfce49bf37eb4abb3ab628
githubscientist <sathish.datascientist@gmail.com>
2026-02-11 18:33:12 +0530

    complex state

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
 import { useState } from "react";
 
-const App = () => {
-  const [likes, setLikes] = useState<number>(0);
-  const [dislikes, setDislikes] = useState<number>(0);
+type Data = {
+  likes: number,
+  dislikes: number,
+  reactions: string[]
+};
 
-  const [reactions, setReactions] = useState<string []>([]);
+const App = () => {
+  const [data, setData] = useState<Data>({
+    likes: 0,
+    dislikes: 0,
+    reactions: []
+  });
 
   const handleLike = () => {
-    setLikes(likes + 1);
-    setReactions([...reactions, 'L']);
+    setData({
+      ...data,
+      likes: data.likes + 1,
+      reactions: [
+        ...data.reactions,
+        'L'
+      ]
+    })
   };
 
   const handleDislike = () => {
-    setDislikes(dislikes + 1);
-    setReactions([...reactions, 'D']);
+    setData({
+      ...data,
+      dislikes: data.dislikes + 1,
+      reactions: [
+        ...data.reactions,
+        'D'
+      ]
+    })
   };
 
-  console.log(reactions);
+  console.log(data);
 
   return (
     <>
-      <button onClick={handleLike}>Like</button> &nbsp; { likes } &nbsp; likes &nbsp;
-      <button onClick={handleDislike}>Dislike</button> &nbsp; { dislikes } &nbsp; dislikes &nbsp;
+      <button onClick={handleLike}>Like</button> &nbsp; { data.likes } &nbsp; likes &nbsp;
+      <button onClick={handleDislike}>Dislike</button> &nbsp; { data.dislikes } &nbsp; dislikes &nbsp;
     </>
   )
 }
